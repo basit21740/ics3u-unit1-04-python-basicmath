bd063a5c6a80e2fc2c12fc4e0125de11cd6637d1
abdul <abdul.butt@mths.ca>
2021-11-09 16:16:48 +0000

    added basic math

---
+++ basic-math.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Abdul Basit 
+# Created on: Nov 2021
+# This program does basic math
+
+
+def main():
+    # this function does basic math
+
+    print(9 + 2)
+    print("7-3={}".format(7 - 3))
+    print("4/2={}".format(4 / 2))
+    print("4+4/2={}".format(4 + 4 / 2))
+    print("3+4*2={}".format(3 + 4 * 2))
+    print("5+2^3={}".format(5 + 2 ^ 3))  # Bitwise Operators
+    print("5+2³={}".format(5 + 2 ** 3))  # exponent
+
+
+if __name__ == "__main__":
+    main()
